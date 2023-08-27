bf7d29ae0ee855731f6b5afec1fbc0a692e5d7aa
Nicolae Rachiu <rachiunicu@gamil.com>
2023-08-27 13:36:02 +0200

    Eliminated Namespace from autoload.php file

--- admin/autoload.php
+++ admin/autoload.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Admin\Classes; // aggiungi la dichiarazione del namespace
-
 spl_autoload_register(function ($class) {
     $class = str_replace('Admin\Classes\\', '', $class);
     $classFilePath = __DIR__ . DS . 'Classes' . DS . $class . '.php';
