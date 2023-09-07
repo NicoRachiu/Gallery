692d72e207be2133cd872a7e3808fb09bad4f7d8
Nicolae Rachiu <rachiunicu@gamil.com>
2023-09-07 15:09:47 +0200

    Created the index and login controller

--- admin/classes/Controller.php
+++ admin/classes/Controller.php
@@ -6,11 +6,28 @@ class Controller
 {
     public function index()
     {
+        include("Admin/header.php");
+        include("includes/sidebar.php");
+        include("includes/footer.php");
+
         $session = new Session();
 
         if (!$session->is_signed_in()) {
             redirect("login");
         }
+
+        $photos = Photos::find_all();
+        foreach ($photos as $photo) {
+        }
+
+        $template = new Template();
+        $template::view(
+            'Templates/login.html',
+            [
+                'Photo_id' => $photo->picture_path(),
+                'colors' => ['red', 'blue', 'green'],
+            ]
+        );
 
         exit;
     }
@@ -23,10 +40,10 @@ class Controller
             $password = trim($_POST['password']);
 
             //Method to check database user
-            $user_found = Admin\Classes\Users::verify_user($username, $password);
+            $user_found = Users::verify_user($username, $password);
 
             if ($user_found) {
-
+                $session = new Session();
                 $session->login($user_found);
                 redirect("index.php");
             } else {
@@ -44,7 +61,9 @@ class Controller
             'Templates/login.html',
             [
                 'username' => $username,
-                'colors' => ['red', 'blue', 'green']
+                'colors' => ['red', 'blue', 'green'],
+                'new_messgae' => $the_message,
+
             ]
         );
 
