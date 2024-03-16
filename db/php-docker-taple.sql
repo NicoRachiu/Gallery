4eb738ad59e1c7cee631998bcad8b0759096ae2c
NICOLAE RACHIU <139007880+NicoRachiu@users.noreply.github.com>
2024-03-16 13:16:05 +0000

    Add Docker-compose

--- db/php-docker-taple.sql
+++ db/php-docker-taple.sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 15, 2024 at 11:30 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `gallery_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
 
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
@@ -35,9 +14,7 @@ CREATE TABLE `comments` (
   `created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `comments`
---
+
 
 INSERT INTO `comments` (`id`, `photo_id`, `author`, `body`, `created`) VALUES
 (22, 81, 'ok1', 'ok', '2023-12-30 12:22:42'),
@@ -46,11 +23,7 @@ INSERT INTO `comments` (`id`, `photo_id`, `author`, `body`, `created`) VALUES
 (25, 81, 'ok', 'ok', '2023-12-30 12:22:42'),
 (29, 0, 'gbgbxg', 'xgbxgb', '2024-01-01 21:57:19');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `photos`
---
 
 CREATE TABLE `photos` (
   `id` int(11) NOT NULL,
@@ -63,19 +36,13 @@ CREATE TABLE `photos` (
   `size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `photos`
---
+
 
 INSERT INTO `photos` (`id`, `title`, `caption`, `description`, `alternative_text`, `filename`, `type`, `size`) VALUES
 (79, 'Macchina blu', '', '', 'ok', 'images-3 copy.jpg', 'image/jpeg', 18096),
 (81, '', '', '', '', 'images-10 copy.jpg', 'image/jpeg', 20401);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -87,60 +54,32 @@ CREATE TABLE `users` (
   `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `user_image`, `email`) VALUES
 (176, 'ok', '', 'ok', 'ok', '_large_image_2.jpg', 'ciao@gmail.com'),
 (212, 'chhncghhcghhm', 'hvhjcghcjgj', 'fhfyhffn', 'fnfjvmcgmcg', '_large_image_2.jpg', '');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `comments`
---
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `photo_id` (`photo_id`);
 
---
--- Indexes for table `photos`
---
+
 ALTER TABLE `photos`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `comments`
---
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
---
--- AUTO_INCREMENT for table `photos`
---
+
 ALTER TABLE `photos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=218;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
